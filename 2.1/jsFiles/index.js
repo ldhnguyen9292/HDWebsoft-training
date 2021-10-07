da2fdf684bed282607a6f95dab1cece1a6b2fd68
LÊ ĐẠI HOÀNG NGUYÊN <45066985+ldhnguyen9292@users.noreply.github.com>
2021-10-07 09:36:23 +0700

    Delete 2.1/jsFiles directory

--- 2.1/jsFiles/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var test_hapi_1 = require("./playround/test-hapi");
-// import { run } from "./playround/agenda";
-// // run job to check database every minutes
-// run()
-// CRUD hapi
-test_hapi_1.init();
--- 2.1/jsFiles/playround/rootRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.root = void 0;
-var hitApiRouter_1 = require("./hitApiRouter");
-var postRouter_1 = require("./postRouter");
-var userRouter_1 = require("./userRouter");
-exports.root = userRouter_1.userRouter.concat(postRouter_1.postRouter, hitApiRouter_1.hitApiRouter);
