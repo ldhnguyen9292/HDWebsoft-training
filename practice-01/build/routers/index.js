b0f42b300923c62da96826a0bedee3f78447d9e4
LÊ ĐẠI HOÀNG NGUYÊN <45066985+ldhnguyen9292@users.noreply.github.com>
2021-10-07 09:37:21 +0700

    Delete practice-01/build directory

--- practice-01/build/routers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rootRouter = void 0;
-var event_router_1 = require("./event.router");
-var voucher_router_1 = require("./voucher.router");
-exports.rootRouter = event_router_1.eventRouter.concat(voucher_router_1.voucherRouter);
